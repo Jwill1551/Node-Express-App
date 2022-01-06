c989c3343416f45168579789de87e1e293ff9f99
Joshua Williams <joshuawilliams1551@gmail.com>
2022-01-06 10:55:16 -0700

    Act1

---
+++ helloworld/app.js
@@ -0,0 +1,8 @@
+// Defualt variables needed to run the application
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World'))
+app.listen(port, () => console.log('Example app listening on port ${port}!'))
+
