8fda981c5921179cd4e8ca304740dccdd67cc64e
Joshua Williams <joshuawilliams1551@gmail.com>
2022-01-11 10:51:35 -0700

    Jade files & debug script added

--- helloworld/app.js
+++ helloworld/app.js
@@ -3,6 +3,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.set('views','./views')
+app.set('view engine','jade')
+
 app.get('/', (req, res) => res.send('Hello World'))
+app.get('/hello', function (_req,res) {
+    res.render('index', {title: 'Hello World', message: 'Hello world!'})
+})
+
+app.use(express.static('public'))
+
 app.listen(port, () => console.log('Example app listening on port ${port}!'))
 
